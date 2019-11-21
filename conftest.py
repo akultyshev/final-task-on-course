5d189757b6cdeeca99dbaf32f923e40fc2ec00fc
akultyshev <easy.nwo@gmail.com>
2019-11-21 15:08:22 +0400

    Add base file for last project

---
+++ conftest.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption("--language", action="store", default = "en", help="Choose ui language:")
+
+
+@pytest.fixture
+def browser(request):
+    user_language = request.config.getoption("language")
+    browser = webdriver.Chrome()
+    yield browser
+    browser.quit()
